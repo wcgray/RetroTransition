d68eaaa3e568a481e2e24801d58e4d3b797220aa
wcgray <cam@tinsoldiersoftware.com>
2019-03-19 11:55:35 -0400

    Initial Commit

---
+++ RetroTransition/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RetroTransition
+//
+//  Created by Campbell Gray on 2019-03-19.
+//  Copyright © 2019 Campbell Gray. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
